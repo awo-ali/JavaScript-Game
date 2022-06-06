886867ae1c68ce60abd2fd2574384f1a86d49563
awo.ali <awoali@hotmail.co.uk>
2022-06-06 10:00:06 +0100

    prize total function

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 //----------------------------Import Data --------------------------------//
 import {questionsArray} from './Data/Questions.js'
 
-/*console.log(questionsArray);
-console.log(questionsArray[0].question);
-console.log(questionsArray[0].content);
-console.log(questionsArray[0].correct); 
-console.log(questionsArray[0].content[2]);
-console.log(questionsArray[0].content[0]);*/
+
 
 //------------------------------Get Elements-------------------------------//
 
@@ -16,130 +11,116 @@ const answerA = document.querySelector(".grid-container__Answer-A")
 const answerB = document.querySelector(".grid-container__Answer-B")
 const answerC = document.querySelector(".grid-container__Answer-C")
 const answerD = document.querySelector(".grid-container__Answer-D")
-//const correctAnswer = document.querySelector(".correctAnswer")
-//const incorrectAnswer = document.querySelector(".incorrectAnswer") 
+const prizeTotal = document.querySelectorAll(".grid-container__prizeTotal")
+const prizeContainer = document.querySelector(".grid-container__prize")
+//const prizeTotalDiv = prizeTotal.getElementByTagName();
+//console.log(prizeTotalDiv);
 
-//console.log();
+console.log(prizeTotal);
+console.log(prizeTotal.length);
+console.log(prizeTotal[14]);
 
-//console.log(questionsArray.length);
 
+  /*const totalPrize = (event) => {
+  for (let i=0; i< prizeTotal.length; i++)
+}*/
 
 
 //-------------------------------Functions-----------------------------------------//
 
-/*const randomQuestionBlock = (array) =>{
-   const randomQuestion = Math.floor(Math.random(questionsArray) * 75)
-   console.log( randomQuestion = Math.floor(Math.random(questionsArray) * 75));
-   return randomQuestion
-}
-
-console.log(randomQuestionBlock(questionsArray));
-console.log(randomQuestion);*/
-/*const randomQuestion = Math.floor(Math.random(questionsArray) * 75)
-const obj = objects[randomQuestion]
-console.log(obj);*/
-
 const randomQuestion = (array) =>{
   const keys = Object.values(questionsArray)
-  //console.log(keys);
   return keys[Math.floor(Math.random(questionsArray) * 75)]
 }
 
-
-
-//console.log(randomQuestion(questionsArray));
-
 const questionAndAnswer = randomQuestion(questionsArray)
-//console.log(questionAndAnswer);
-//console.log(questionAndAnswer.correct);
-
-//console.log(randomQuestion(questionsArray).content);
-//console.log(randomQuestion(questionsArray).content[2]);
-
-
-
-const addAnswers = (event) => {
-    answerBlock.innerHTML = `
-        <section class="grid-container__Answer">
-          <div class="grid-container__Answer-blockOne">
-            <div class="grid-container__Answer-A">A: ${questionAndAnswer.content[0]}</div>
-            <div class="grid-container__Answer-C">C: ${questionAndAnswer.content[2]}</div>
-          </div>
-          <div class="grid-container__Answer-blockTwo">
-            <div class="grid-container__Answer-B">B: ${questionAndAnswer.content[1]}</div>
-            <div class="grid-container__Answer-D">C: ${questionAndAnswer.content[3]}</div>
-          </div>
-        </section>`
-};
-
 
 const addRandomQuestion = (event) => {
     questionBlock.innerHTML = 
     ` <section class="grid-container__Question"> ${questionAndAnswer.question} </section>`
 };
 
-console.log(addRandomQuestion);
+
+const addAnswers = () => {
+  answerA.innerText = `A: ${questionAndAnswer.content[0]}`
+  answerC.innerText = `B: ${questionAndAnswer.content[2]}`
+  answerB.innerText = `C: ${questionAndAnswer.content[1]}`
+  answerD.innerText = `D: ${questionAndAnswer.content[3]}`
+};
+
+const handleClick = (event) => {
+  if(event.target.id === "grid-container__AnswerButton-A") {
+    answerCorrect(answerA, 0)
+  } else if (event.target.id === "grid-container__AnswerButton-B"){
+    answerCorrect(answerB, 1)
+  } else if (event.target.id === "grid-container__AnswerButton-C"){
+    answerCorrect(answerC, 2)
+  } else if (event.target.id === "grid-container__AnswerButton-D"){
+    answerCorrect(answerD, 3)
+  }
+  
+};
+
+const answerCorrect = (answer, index) => {
+  if (index === questionAndAnswer.correct) {
+    answer.classList.add("correctAnswer");
+  } else {
+    answer.classList.add("incorrectAnswer");
+  }
+  
+};
+
+
+
+console.log(answerCorrect);
 console.log(questionAndAnswer.correct);
 
-const answerCorrect = (answer, index) => {
-  if (index === questionAndAnswer.correct){
-    answer.classList.add('correctAnswer') 
-  } else {
-    answer.classList.add('incorrectAnswer') 
-  }
-  console.log(answerCorrect);
-  console.log(answer);
-  
+const clearAnswer = (answer, index) => {
+  answer.classList.remove("correctAnswer");
+  answer.classList.remove("incorrectAnswer");
+}
+
+const clearAnswerDelay = async (clearAnswer) => {
+await delay(5000);
+console.log(clearAnswer);
 };
 
-/*const answerCorrect = (answer, index) => {
-  if (index === questionAndAnswer.correct){
-    answer.style.backgroundColor = 'green';
-  } else {
-  answer.style.backgroundColor = 'red';
+const totalPrize = (event) => {
+  for (let i=prizeTotal.length -1; i >= 0; i--){
+    if (questionAndAnswer.correct){
+      [i].classList.add("prizeScore");
+    }else{
+      prizeTotal[14].classList.add("prizeScore");
+    }
   }
-  console.log(answerCorrect);
-  console.log(answer);
-  
+  console.log(totalPrize);
+}
+
+/*const totalPrize = (event) => {
+  if (index === questionAndAnswer.correct){
+    for(let i = 0; i < prizeTotal.length)
+  }
+}*/
+
+/*const totalPrize = (event) => {
+  if (questionAndAnswer.correct){
+    [...prizeTotal].forEach((div, index) =>{
+      div.classList.add("prizeScore");})
+ } else {
+      //div.classList.add("prizeScore");
+      return
+    }
+    
 };*/
-
-/*const answerCorrect = (answer, index) => {
-  if (index === questionAndAnswer.correct){
-    answer.classList.toggle('correctAnswer') 
-  } else {
-    answer.classList.toggle('incorrectAnswer') 
-  }
-  console.log(answerCorrect);
-  console.log(answer);
-  
-};
-const answerCorrect = (answer, index) => {
-  if (index === questionAndAnswer.correct){
-    document.getElementById("overlay-1").style.display = "show"; 
-  } else {
-    document.getElementById("overlay-2").style.display = "block"
-  }
-  
-}*/
-
-
-/*const hover = (answer) =>{
-  answer.classList.add('hoverAnswer') 
-}*/
-
-
-console.log();
 
 //-----------------------------------------Return Function------------------------------------------//
 
 
 window.addEventListener("load", addAnswers)
-answerA.addEventListener("click", answerCorrect(answerA,0))
-answerB.addEventListener("click", answerCorrect(answerB,1))
-answerC.addEventListener("click", answerCorrect(answerC,2))
-answerD.addEventListener("click", answerCorrect(answerD,3))
 window.addEventListener("load", addRandomQuestion)
-/*answerA.addEventListener("mouseover", hover(answerA))
-answerB.addEventListener("mouseover", hover(answerB))
-answerC.addEventListener("mouseover", hover(answerC))
-answerD.addEventListener("mouseover", hover(answerD))*/
+answerA.addEventListener("click", handleClick);
+answerB.addEventListener("click", handleClick);
+answerC.addEventListener("click", handleClick);
+answerD.addEventListener("click", handleClick);
+window.addEventListener("load", totalPrize);
+window.addEventListener("load", clearAnswer);
